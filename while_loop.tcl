fb96b97f56207c637c4fd534a82b526ecfb0f915
sohamxda7 <sensoham135@gmail.com>
2024-02-16 11:42:01 +0530

    while_loop: optimize the variables
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- while_loop.tcl
+++ while_loop.tcl
@@ -1,11 +1,11 @@
 #!/usr/bin/tclsh
 
 puts "Enter number"
-gets stdin a
-while {![string is integer -strict $a]} {
+gets stdin usernum
+while {![string is entier -strict $usernum]} {
 puts "Please enter a valid integer"
-gets stdin a}
-set b [expr {$a * $a}]
-puts "The square of your integer is: $b"
+gets stdin usernum}
+set result [expr {$usernum * $usernum}]
+puts "The square of your integer is: $result"
 
 
