cf634e21f02490f638d2cf635c8362fb2c28b795
sohamxda7 <sensoham135@gmail.com>
2024-02-17 14:38:44 +0530

    for_loop: optimize the check logic
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- for_loop.tcl
+++ for_loop.tcl
@@ -1,11 +1,17 @@
 #!/usr/bin/tclsh
 
+#while & if number check
+
+while {true} {
 puts "Enter the number:"
 gets stdin usernum
-while {![string is entier -strict $usernum]} {
-puts "please enter a valid integer number again!"
-puts "Enter the number:"
-gets stdin usernum }
+if {[string is entier -strict $usernum]} {
+	break
+} else {
+	puts "Please enter the value again!"
+	after 1000}}
+
+#implementation of for_loop	
 for {set multnum 1} {$multnum<=10} {incr multnum} {
 set output [expr {$usernum * $multnum}]
 puts [format "%d X %d = %d" $usernum $multnum $output]
