3f83f0c080f446183317cdeb72032daaf1c4d662
sohamxda7 <sensoham135@gmail.com>
2024-02-17 14:47:48 +0530

    while_loop: fix the verify logic
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- while_loop.tcl
+++ while_loop.tcl
@@ -1,10 +1,13 @@
 #!/usr/bin/tclsh
 
-puts "Enter number"
+while {true} {
+puts "Enter a number:"
 gets stdin usernum
-while {![string is entier -strict $usernum]} {
-puts "Please enter a valid integer"
-gets stdin usernum}
+if {[string is double -strict $usernum]} {
+	break
+} else {
+	puts "Please enter a valid number"
+	after 1000 } }
 set result [expr {$usernum * $usernum}]
 puts "The square of your integer is: $result"
 
