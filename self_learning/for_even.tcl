71597733253ec3443ef59350461f965f31093821
sohamxda7 <sensoham135@gmail.com>
2024-02-19 14:22:43 +0530

    for_even: fix the var init
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- self_learning/for_even.tcl
+++ self_learning/for_even.tcl
@@ -2,6 +2,7 @@
 
 #Write a Tcl script that calculates the sum of all even numbers between 1 and 100.
 
+set sum  0
 for {set num 2} {$num <= 100} {incr num 2} {
-	puts "Even: $num"
-	after 1000 } 
+	set sum [expr { $sum + $num }]}
+puts "Sum of all even number from 1 to 100 is: $sum"
