a3ca39ccb874329c7839272a3bc7da990c229295
sohamxda7 <sensoham135@gmail.com>
2024-02-21 12:02:34 +0530

    tcl: learning arrays
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

---
+++ self_learning_array/array_1.tcl
@@ -0,0 +1,47 @@
+#!/usr/bin/tclsh
+
+array set fruits {
+	apple 3
+	banana 5
+	orange 2
+}
+
+puts $fruits(banana)
+
+puts "\n"
+
+array set colors {
+	red #FF0000
+	green #00FF00
+	blue #0000FF
+}
+
+foreach {key value} [array get colors] {
+	puts "Key: $key, Value: $value"
+}
+
+puts "\n"
+
+array set shoppingList {
+	apples 5
+	bananas 3
+	milk 2
+}
+
+puts "[array names shoppingList]"
+puts "[array size shoppingList]"
+
+puts "\n"
+
+array set shopping {
+	apples 5
+	bananas 3
+	milk 2
+}
+
+set shopping(apples) 8
+set shopping(bread) 1
+
+foreach {key value} [array get shopping] {
+	puts "Item: $key, Qty: $value"
+}
