7e6db4f90d84a216fa1ec7132c913e0eff1863ac
sohamxda7 <sensoham135@gmail.com>
2024-02-28 15:48:33 +0530

    tcl: rewrite and add tcl stuff
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

---
+++ Question_Set_2_Reboot/array_2.tcl
@@ -0,0 +1,33 @@
+#!/usr/bin/tclsh
+
+#Write a TCL script to merge two arrays of the same size and sort in descending order.
+
+
+#set two arrays
+array set first_colorcount {
+    red   2
+    green 5
+    blue  4
+    white 9
+}
+
+array set second_colorcount {
+	yellow 3
+	brown 6
+	violet 7
+	black 10
+}
+
+#merge two arrays
+if {[array size first_colorcount] == [array size second_colorcount]} {
+	foreach {color value} [array get first_colorcount] {
+		 set second_colorcount($color) $value
+}
+
+#main logic
+	foreach {color count} [lsort -decreasing -stride 2 -index 1 -integer [array get second_colorcount]] {
+    			puts "Color: $color & Count: $count"
+	} 
+} else {
+	puts "Two arrays are not of same size!"
+}
---
+++ Question_Set_2_Reboot/array_4.tcl
@@ -0,0 +1,20 @@
+#!/usr/bin/tclsh
+
+#Write a TCL script to insert the values in the array. (
+
+#set the array
+array set firstarr {
+	0 2
+	1 5
+	2 7
+	3 9
+	4 11
+}
+
+#insert a value
+set firstarr(5) 8
+
+#sort
+foreach {key value} [lsort -increasing -stride 2 -index 1 -integer [array get firstarr]] {
+	puts "$value"
+}
