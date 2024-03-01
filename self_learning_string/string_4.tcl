a32d51c199751bebac610af70bf371194d945cfa
sohamxda7 <sensoham135@gmail.com>
2024-03-01 11:28:21 +0530

    tcl: self_learning assignment string_4
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

---
+++ self_learning_string/string_4.tcl
@@ -0,0 +1,9 @@
+#!/usr/bin/tclsh
+
+#Write a script that takes a string as input and prints it in reverse order.
+
+puts "Enter a sentence"
+gets stdin str
+puts "In reverse: [string reverse $str]"
+
+
