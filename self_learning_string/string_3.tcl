89534d16b4249426dff21a9eadf007ab50e49147
sohamxda7 <sensoham135@gmail.com>
2024-03-01 11:12:59 +0530

    tcl:self_learning assignment string_3
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

---
+++ self_learning_string/string_3.tcl
@@ -0,0 +1,9 @@
+#!/usr/bin/tclsh
+
+#Create a script that takes two strings as input and concatenates them together, separating them with a space.
+
+puts "Enter the sentence:"
+gets stdin str
+puts "Enter another sentence:"
+gets stdin strn
+puts "$str $strn"
