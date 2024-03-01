4c150165655c9e712cdc76ff281439c6204c70b5
sohamxda7 <sensoham135@gmail.com>
2024-03-01 10:04:34 +0530

    tcl: self_learning string_1 exercise
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

---
+++ self_learning_string/string_1.tcl
@@ -0,0 +1,7 @@
+#!/usr/bin/tclsh
+
+#Create a Tcl script that takes a string as input and prints its length.
+
+puts "Enter the string"
+gets stdin str
+puts "The string length is: [string length $str]"
